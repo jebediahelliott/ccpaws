b8679575cea813e0dfb2f9c62c37cce1334bd6ea
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-12 23:17:25 +0000

    finish Scraper tests

--- lib/ccpaws/scraper.rb
+++ lib/ccpaws/scraper.rb
@@ -1,23 +1,23 @@
 class Ccpaws::Scraper
 
-  def self.scrape_index_page
+  def self.scrape
     html = open("https://www.meetup.com/find/tech/?allMeetups=false&radius=10&userFreeform=State+College%2C+PA&mcId=c16801&mcName=State+College%2C+PA&sort=default")
     doc = Nokogiri::HTML(html)
     group_cards = []
     cards = doc.css(".groupCard")
     cards.each do |card|
-      group_cards << Ccpaws::Group.newI({
-      nane: card.css("h3").text.strip,
+      group_cards << Ccpaws::Group.new({
+      name: card.css("h3").text.strip,
       members: card.css("p").text.strip,
       url: card.css("a").attribute("href").value
     })
-    binding.pry
     end
 
     group_cards.each do |card|
       html = open(card.url)
       doc = Nokogiri::HTML(html)
-      card.description = doc.css(".group-description .chunk p")
+      card.description = doc.css(".group-description .chunk p").text
     end
+    binding.pry
   end
 end
