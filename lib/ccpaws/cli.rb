4384ca669400264ef442c21bd6cdc9e372097094
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-12 23:22:41 +0000

    start setting up cli to take real values

--- lib/ccpaws/cli.rb
+++ lib/ccpaws/cli.rb
@@ -7,9 +7,7 @@ class Ccpaws::CLI
 
   def list
     puts "Welcome! Here is a list of dogs available for adoption at Centre County Paws"
-    puts "1. Ballpark Frank: Male, Coonhound, Adult, Large"
-    puts "2. Beaufort: Male, Beagle/Terrier, Adult, Small"
-    puts "3. Brandi: Female, Labrador Retriever/Husky, Senior, Large"
+    
   end
 
   def menu
--- lib/ccpaws/dog.rb
+++ lib/ccpaws/dog.rb
@@ -10,15 +10,6 @@ class Ccpaws::Group
   def initialize(group_hash)
   group_hash.each{ |key, value| self.send(("#{key}="), value)}
   @@all << self
-end
-
-def self.create_from_collection(group_array)
-  group_array.each{ |hash| self.new(hash)}
-end
-
-def add_student_attributes(page_hash)
-  page_hash.each{ |key, value| self.send(("#{key}="), value)}
-  self
-end
+  end
 
 end
