1c83413fed46453952e6bf509ee599fdcb527534
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-13 11:44:50 +0000

    rename files

---
+++ lib/state_college_tech_meets.rb
@@ -0,0 +1,10 @@
+require_relative "./state_college_tech_meets/version"
+require_relative "./state_college_tech_meets/cli"
+require_relative "./state_college_tech_meets/meetup/"
+require_relative "./state_college_tech_meets/scraper"
+require "nokogiri"
+require "pry"
+require "open-uri"
+module Ccpaws
+  # Your code goes here...
+end
