ee623ce113aa1ea4d8385a5ad84950b6713f63b1
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-13 01:06:04 +0000

    set up menu to show description

--- lib/ccpaws/cli.rb
+++ lib/ccpaws/cli.rb
@@ -2,37 +2,33 @@ class Ccpaws::CLI
   def call
     Ccpaws::Scraper.scrape
     list
-    # menu
+    menu
+    goodbye
   end
 
   def list
-    puts "Welcome! Here are the tech Meetups in the State college area"
-    Ccpaws::Group.all.each_with_index do |meetup, i|
-      puts "#{i + 1}. #{meetup.name} #{meetup.members}"
+    puts "Welcome! Here are the tech Meetups in the State College area"
+    Ccpaws::Group.all.each.with_index(1) do |meetup, i|
+      puts "#{i}. #{meetup.name} #{meetup.members}"
     end
   end
 
-  # def menu
-  #
-  #   input = nil
-  #   while input != "exit"
-  #     puts "Please choose a dogs number to see more information. Type list to return to list or type exit."
-  #     input = gets.strip.downcase
-  #     case input
-  #     when "1"
-  #       puts "Ballpark Frank..."
-  #     when "2"
-  #       puts "Beaufort..."
-  #     when "3"
-  #       puts "Brandi..."
-  #     when "list"
-  #       list
-  #     end
-  #   end
-  #   goodbye
-  # end
-  #
-  # def goodbye
-  #   puts "Thanks for checking out our furry friends!"
-  # end
+  def menu
+
+    input = nil
+    while input != "exit"
+      puts "Please choose a Meetup to see more information or type exit."
+      input = gets.strip.downcase
+      if input > 0 && input <= Ccpaws::Group.all.length
+        group = Ccpaws::Group.all[input.to_i -1]
+        puts "#{group.description}"
+        puts "Type list to go back to the menu or type exit."
+
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thanks for checking out our furry friends!"
+  end
 end
