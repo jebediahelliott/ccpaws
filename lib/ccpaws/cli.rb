d915e9ca8c6d1f9ce46aa0e4085e5257cac4b597
Jebediah Elliott <jebediahelliott@gmail.com>
2018-06-13 01:21:20 +0000

    debug and finalize project

--- lib/ccpaws/cli.rb
+++ lib/ccpaws/cli.rb
@@ -1,34 +1,38 @@
 class Ccpaws::CLI
   def call
     Ccpaws::Scraper.scrape
-    list
     menu
+    more_info
     goodbye
   end
 
-  def list
+  def menu
     puts "Welcome! Here are the tech Meetups in the State College area"
     Ccpaws::Group.all.each.with_index(1) do |meetup, i|
       puts "#{i}. #{meetup.name} #{meetup.members}"
     end
   end
 
-  def menu
+  def more_info
 
     input = nil
     while input != "exit"
-      puts "Please choose a Meetup to see more information or type exit."
+      puts "Please choose a Meetup to see more information, type menu to return to the menu or type exit."
       input = gets.strip.downcase
-      if input > 0 && input <= Ccpaws::Group.all.length
-        group = Ccpaws::Group.all[input.to_i -1]
+      if input.to_i > 0 && input.to_i <= Ccpaws::Group.all.length
+        group = Ccpaws::Group.all[input.to_i - 1]
         puts "#{group.description}"
-        puts "Type list to go back to the menu or type exit."
-
+      elsif input == "menu"
+        menu
+      elsif input == "exit"
+        break
+      else
+        puts "Invalid entry, please type menu or exit"
       end
     end
   end
 
   def goodbye
-    puts "Thanks for checking out our furry friends!"
+    puts "Have fun with your new friends!!"
   end
 end
